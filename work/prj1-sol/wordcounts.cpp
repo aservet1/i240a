47d5ebbe9391ec4e3dea6c9fe8941699513496eb
Alejandro Servetto <aservet1@cs.binghamton.edu>
2020-02-15 20:47:08 -0500

    more work, getting questionable output

--- work/prj1-sol/wordcounts.cpp
+++ work/prj1-sol/wordcounts.cpp
@@ -1,13 +1,15 @@
 #include<iostream>
 #include<unordered_map>
 #include<fstream>
+#include<vector>
+#include<algorithm>
 
 static void
 print_map(std::unordered_map<std::string,int> &map)
 {
 	for (auto it = map.cbegin(); it != map.cend(); ++it)
-		std::cout << "{" << (*it).first << ": "
-			<< (*it).second << "}" << std::endl;
+		std::cout << (*it).first << ": "
+			<< (*it).second << std::endl;
 }
 
 static std::string
@@ -20,41 +22,68 @@ fix_word(std::string inWord)
 	return outWord;
 }
 
-std::unordered_map<std::string,int>
-count_file_words(int max_n_out, int min_word_length, int max_word_length, std::string filename)
+std::ifstream
+open_file_check_error(std::string fileName)
 {
-	std::unordered_map<std::string, int> wordcounts;
 	std::ifstream inFile;
-	std::string word;
-
-	inFile.open(filename);
+	inFile.open(fileName);
 	if (!inFile) {
-		std::cerr << "error opening file \"" << filename << "\"" << std::endl
+		std::cerr << "error opening file \"" << fileName << "\"" << std::endl
 			<< "exiting program" << std::endl;
 		exit(1);
 	}
+	return inFile;
+}
+
+bool is_in(std::string key, std::vector<std::string> vect)
+{
+	for (unsigned long i = 0; i < vect.size(); i++)
+		if(vect[i] == key) return true;
+	return false;
+}
+
+bool wordCompare()
+{
+	return false;
+}
+
+std::unordered_map<std::string,int>
+count_file_words(int max_n_out,
+		 int min_word_length,
+		 int max_word_length,
+		 std::string fileName)
+{
+	std::unordered_map<std::string, int> wordcounts;
+	std::string word;
+
+	std::ifstream inFile = open_file_check_error(fileName);
 
 	int word_length;
-	int n_words_written = 0;
+	std::vector<std::string> words_written;
 	while (inFile >> word) {
 		word = fix_word(word);
 		word_length = word.length();
-		if (word_length >= min_word_length &&
-		    word_length <= max_word_length &&
-		    n_words_written <= max_n_out)
+		if (word_length >= min_word_length
+		 && word_length <= max_word_length)
 		{
-			n_words_written += 1;
-			wordcounts[word] += 1;
+			if (is_in(word,words_written))
+				wordcounts[word] += 1;
+			else {
+				if ((int)words_written.size() < max_n_out) {
+					words_written.push_back(word);
+					wordcounts[word] += 1;
+				}
+			}
 		}
 	}
-
+	inFile.close();
 	return wordcounts;
 }
 
 int main(int argc, char *argv[])
 {
 	std::unordered_map wordcounts =
-		count_file_words(10, 5, 5, "lab0.umt");
+		count_file_words(10, 5, 6, "lab0.umt");
 
 	print_map(wordcounts);
 }
