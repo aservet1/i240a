178864caf6989ef39abf8391042fea0d0656cc5f
Alejandro Servetto <aservet1@binghamton.edu>
2020-02-22 13:41:55 -0500

    finsihed exercise 1

--- work/lab4/exercises/1-point/main.cc
+++ work/lab4/exercises/1-point/main.cc
@@ -1,22 +1,23 @@
 #include "point.hh"
+#include "line.hh"
 
 #include <iostream>
 
 //declare an array of points
 static const Point points[] = {
-  Point(0, 0), Point(3, 4), Point(1, 1), Point(4, 5),
+  Point(0, 0), Point(3, 4), Point(1, 1), Point(4, 5), Point(1,7)
 };
 //use standard idiom to figure out number of elements in array
 static constexpr auto nPoints = sizeof(points)/sizeof(points[0]);
 
-static void
+/*static void
 outSizes(std::ostream& out) {
   out << "sizeof(double) = " << sizeof(double) << std::endl;
   out << "sizeof(Point) = " << sizeof(Point) << std::endl;
-}
+}*/
 
 /** output pair-wise distance between all points in points[] to out. */
-static void
+/*static void
 outDistances(std::ostream& out)
 {
   for (auto i = 0u; i < nPoints - 1; i++) {
@@ -29,16 +30,24 @@ outDistances(std::ostream& out)
 	  << std::endl;
     }
   }
-}
+}*/
 
 int main() {
+
+  for (int i = 0; i < (int)nPoints; i+=2)
+  {
+    Point p1 = points[i];
+    Point p2 = points[i+1];
+    Line line = Line(p1,p2);
+    std::cout << line.toString() << std::endl;
+  }
+
+
 
   //instead of hardcoding std::cout in functions, we pass it as a param.
   //so if we wanted to output to a file, the code for the functions
   //would not need to change (the code in this function would change).
-  std::ostream& out = std::cout;
-
+  /*std::ostream& out = std::cout;
   outDistances(out); out << std::endl;
-
-  outSizes(out);
+  outSizes(out);*/
 }
